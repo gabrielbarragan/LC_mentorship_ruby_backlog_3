fb4cb2e9ba15c58da2b725208c6089d194fad569
Gabriel Rondon <gabrielbarragan07@gmail.com>
2022-06-11 15:03:51 -0500

    hashes terminado

--- ruby_hashes.rb
+++ ruby_hashes.rb
@@ -5,4 +5,30 @@ catalogo_tienda = {
     "pants" => 2
 }
 
-puts catalogo_tienda
+#precio de la camisa
+precio_camisa = catalogo_tienda["camisa"]
+
+puts 'precio de la camisa: '+ precio_camisa.to_s
+
+#Variable 'new_hash'
+new_hash = {}
+
+#Variable 'canasta_de_frutas'
+canasta_de_frutas = {
+    'limón' => 3,
+    'banano' => 10,
+    'Mango' => 5
+}
+
+#Agregar una fruta
+
+canasta_de_frutas['melón'] = 4
+
+#Variable 'fruta seleccionada'
+
+fruta_seleccionada = canasta_de_frutas['banano']
+
+puts 'fruta seleccionada: '+fruta_seleccionada.to_s
+
+
+
